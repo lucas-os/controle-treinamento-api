3924941ca0d911f2b8725f294a603843c4954c15
Lucas Oliveira Silvério <oliveiratricolorfc@hotmail.com>
2026-01-15 18:21:02 -0300

    Projeto Criado

---
+++ src/main/java/com/lucas/treinamento/ControleTreinamentoApiApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.treinamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleTreinamentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleTreinamentoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/treinamento/ControleTreinamentoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.treinamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleTreinamentoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
